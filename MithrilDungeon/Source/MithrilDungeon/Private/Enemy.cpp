d378cbbc314cf13d5b4de28cf1e6c17d0a751b23
jjj12g <159858734+jjj12g@users.noreply.github.com>
2024-05-23 19:32:35 +0900

    에너미 공격애니메이션 적용, 소켓에 무기장착

---
+++ MithrilDungeon/Source/MithrilDungeon/Private/Enemy.cpp
@@ -0,0 +1,154 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy.h"
+#include "AIController.h"
+#include "../MithrilDungeonCharacter.h"
+#include "NavigationSystem.h"
+#include "NavigationPath.h"
+#include "NavigationInvokerComponent.h"
+#include "EngineUtils.h"
+#include "Animation/AnimMontage.h"
+#include <../../../../../../../Source/Runtime/Engine/Classes/Animation/AnimInstance.h>
+#include <../../../../../../../Source/Runtime/Engine/Classes/Components/StaticMeshComponent.h>
+#include <../../../../../../../Source/Runtime/Engine/Classes/Components/BoxComponent.h>
+
+
+// Sets default values
+AEnemy::AEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 무기
+	swordComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SwordComp"));
+	swordComp->SetupAttachment(GetMesh(), TEXT("b_RightHandSocket"));
+	swordComp->SetRelativeLocation(FVector(11.577815, -3.876722, 5.406297));
+	swordComp->SetRelativeRotation(FRotator(36.225576, 100.412465, -48.868406));
+
+	// 무기날, 오버랩되면 데미지
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("boxComp"));
+	boxComp->SetupAttachment(swordComp);
+	boxComp->SetRelativeLocation(FVector(-0.000001, 0, 82.601549));
+	boxComp->SetRelativeScale3D(FVector(0.1,0.1,2.2));
+}
+
+
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 월드에 있는 플레이어를 찾는다.
+	for (TActorIterator<AMithrilDungeonCharacter> findActor(GetWorld()); findActor; ++findActor)
+	{
+		Player = *findActor;
+	}
+
+	aiCon = GetController<AAIController>();
+
+	// 기본 상태를 IDLE 상태로 초기화한다.
+	enemyState = EEnemyState::IDLE;
+	
+}
+
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	switch (enemyState)
+	{
+	case EEnemyState::IDLE:
+		Idle();
+		break;
+	case EEnemyState::MOVE:
+		MoveTotaget();
+		break;
+	case EEnemyState::ATTACK:
+		Attack();
+		break;
+	case EEnemyState::ATTACKDELAY:
+		
+		break;
+	case EEnemyState::RETURN:
+	
+		break;
+	case EEnemyState::DAMAGED:
+		
+		break;
+	case EEnemyState::DIE:
+		//Die();
+		break;
+	default:
+		break;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("State Transition: %s"), *StaticEnum<EEnemyState>()->GetValueAsString(enemyState));
+}
+
+void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AEnemy::Idle()
+{
+	SearchPlayer();
+	
+
+}
+
+void AEnemy::MoveTotaget()
+{
+	FVector targetLoc = Player->GetActorLocation();
+	if (aiCon != nullptr)
+	{
+		if (FVector::Distance(GetActorLocation(), targetLoc) < limitDistance && FVector::Distance(GetActorLocation(),targetLoc) > attackDistance)
+		{ 
+			UE_LOG(LogTemp, Warning, TEXT("move!!"));
+			aiCon->MoveToActor(Player);
+		}
+		if (FVector::Distance(GetActorLocation(), targetLoc) <= attackDistance)
+		{
+			enemyState = EEnemyState::ATTACK;
+		}
+	}
+}
+
+void AEnemy::Attack()
+{
+	/*UE_LOG(LogTemp, Warning, TEXT("Attack!!"));*/
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+ 	if (AnimInstance && !bAttack)
+ 	{
+		PlayAnimMontage(attack_Montage);
+		UE_LOG(LogTemp, Warning, TEXT("AttackAM!!"))
+		bAttack = true;
+	}
+	
+}
+
+
+
+
+void AEnemy::OnDamaged(int32 dmg)
+{
+	
+}
+
+void AEnemy::SearchPlayer()
+{
+	UE_LOG(LogTemp, Warning, TEXT("sug"));
+
+	FVector Start = GetActorLocation();
+	FVector End = Player->GetActorLocation();
+	if (FVector::Distance(Start, End) < 100)
+	{
+		enemyState = EEnemyState::IDLE;
+	}
+	else
+	{ 
+		enemyState = EEnemyState::MOVE;
+	}
+}
+
+
+
---
+++ MithrilDungeon/Source/MithrilDungeon/Public/Enemy.h
@@ -0,0 +1,92 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Enemy.generated.h"
+
+UENUM(BlueprintType)
+enum class EEnemyState : uint8
+{
+	IDLE,		
+	MOVE,		
+	ATTACK,		
+	ATTACKDELAY,	
+	RETURN,		
+	DAMAGED,		
+	DIE			
+};
+
+
+UCLASS()
+class MITHRILDUNGEON_API AEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	AEnemy();
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	EEnemyState enemyState = EEnemyState::IDLE;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	class UStaticMeshComponent* swordComp;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	class UBoxComponent* boxComp;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	float traceSpeed = 750.0f;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	float attackDistance = 170.0f;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	float attackDelayTime = 1.5f;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	float limitDistance = 1500.0f;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	float returnSpeed = 1000.0f;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	int32 EnemyMaxHP = 100.0f;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	int32 EnemyCurrentHP = 0;
+
+	void OnDamaged(int32 dmg);
+
+	void SearchPlayer();
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	class UAnimMontage* attack_Montage;
+
+	UPROPERTY(EditAnywhere, Category = "MySettings")
+	class UAnimMontage* death_Montage;
+
+	void Idle();
+	void MoveTotaget();
+	void Attack();
+
+	bool bAttack = false;
+private:
+	/*UPROPERTY()
+	class UEnemyAnimInstance* anim;*/
+
+	class AAIController* aiCon;
+
+	UPROPERTY()
+	class AMithrilDungeonCharacter* Player;
+
+	UPROPERTY()
+	class AActor* target;
+};
