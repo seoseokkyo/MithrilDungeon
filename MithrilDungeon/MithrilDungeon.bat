7b96d11c7a093e0ab2fd5d85c468972d2b069f0b
jjj12g <159858734+jjj12g@users.noreply.github.com>
2024-06-14 09:36:48 +0900

    멀티 되도록 업데이트(메모장편집)

--- MithrilDungeon/MithrilDungeon.bat
+++ MithrilDungeon/MithrilDungeon.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Users\admin\Documents\GitHub\MithrilDungeon\MithrilDungeon.uproject" -game -ResX=600 -ResY=400 -WinX =0 -WinY = 100 -log"
+"C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Users\admin\Documents\GitHub\MithrilDungeon\MithrilDungeon\MithrilDungeon.uproject" -game -ResX=600 -ResY=400 -WinX =0 -WinY = 100 -log"
 
